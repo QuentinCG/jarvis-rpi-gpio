25496462dfd3567415777f0207390a47cad55232
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-02 11:23:54 +0200

    install: Install all dependencies and import python script

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
-#!/bin/bash
-# Use only if you need to perform changes on the user system such as installing software
+#!/usr/bin/env bash
+
+# Install Python (python 2.7 & 3.x)
+[[ -z $(which python) ]] && sudo apt-get --yes --force-yes install python
+[[ -z $(which python2.7) ]] && sudo apt-get --yes --force-yes install python2.7
+[[ -z $(which python3) ]] && sudo apt-get --yes --force-yes install python3
+
+# Install pip
+[[ -z $(which python-pip) ]] && sudo apt-get --yes --force-yes install python-pip
+[[ -z $(which python3-pip) ]] && sudo apt-get --yes --force-yes install python3-pip
+
+# Install & Upgrade Raspberry Pi GPIO python library
+[[ -z $(which python-rpi.gpio) ]] && sudo apt-get --yes --force-yes install python-rpi.gpio
+[[ -z $(which python3-rpi.gpio) ]] && sudo apt-get --yes --force-yes install python3-rpi.gpio
+sudo pip2 install --upgrade RPi.GPIO
+sudo pip3 install --upgrade RPi.GPIO
+
+# Copy latest version of gpio read-write python script
+wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/Raspberry_Pi/utils/readWriteGpio.py
